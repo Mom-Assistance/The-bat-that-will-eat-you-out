88f04eb2b3665c72a82efc877fe0a3eb2ca751c7
Mom Assistance <assistancemom@gmail.com>
2017-04-30 20:21:29 -0400

    ba

---
+++ TTYWO.bat
@@ -0,0 +1,75 @@
+echo off
+cd c:\
+title the bat that will eat you out
+color 4f
+cls
+echo welcome
+pause
+echo this is my way of finding our ip
+pause
+echo first thing is first
+pause 
+echo im eat you out nibba
+pause
+cd c:\users\Public
+color a
+tree
+cls
+color 4f
+echo i found something
+echo im going to ping my backup pc now
+pause
+ping 8.8.8.8
+echo lol jk i just eat you out
+pause 
+cls
+echo look it didnt take long %time%
+pause
+color a
+title hacking time
+cls
+echo Its time to find your porn
+pause
+color 4f
+cls
+echo so im going to look at all the .jpg files on your pc
+pause
+color a
+cd c:\user\public
+tree
+color 4f
+cls
+echo i found some...    imm going to send it to my pc 
+pause
+md c:\user\public\Networkshare
+ping 8.8.8.8
+echo copying.
+echo copying..
+echo copying...
+pause
+color a
+cls
+ping 8.8.8.8
+cls
+color a
+cls
+echo wow this is bad
+pause
+cls
+echo i like things like yiff so this is shit
+pause
+cls
+echo kill your self
+pause
+cls
+color 4f
+cls
+echo so i want you to email "this was a good joke" to assistancemom@gmail.com
+pause
+echo so i want you to email "this was a good joke" to assistancemom@gmail.com
+pause
+echo so i want you to email "this was a good joke" to assistancemom@gmail.com
+pause
+echo only if you think that this was a good joke
+pause
+exit
